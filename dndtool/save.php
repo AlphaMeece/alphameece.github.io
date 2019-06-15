c7885abd2ef9e9776a38cf6d7c5c13ac101da23b
AlphaMeece <33642241+AlphaMeece@users.noreply.github.com>
2019-06-15 02:10:20 -0700

    Update save.php

--- dndtool/save.php
+++ dndtool/save.php
@@ -1,11 +1,7 @@
-<html>
-    <body>
-        <?php
-        $file = fopen("chars/" . $_GET["pName"] . ".txt","w");
-        fwrite($file,$_GET["pName"]);
-        fwrite($file,$_GET["cName"]);
-        fclose($file);
-        echo "Done";
-        ?>
-    </body>
-</html>
+<?php
+$file = fopen("chars/" . $_GET["pName"] . ".txt","w");
+fwrite($file,$_GET["pName"]);
+fwrite($file,$_GET["cName"]);
+fclose($file);
+echo "Done";
+?>
